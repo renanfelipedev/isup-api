353c015d1db6d23266d30bbeb4775dee41b21a9e
Renan Felipe <renan.fb.dantas@gmail.com>
2021-04-09 11:30:41 -0300

    improve code

--- start/routes.ts
+++ start/routes.ts
@@ -20,17 +20,12 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+const auth = { '*': ['auth'] }
+
+Route.get('/', () => ({ server: 'running' }))
 
 Route.post('/login', 'AuthController.login')
 Route.post('/register', 'AuthController.register')
 
-Route.resource('/sites', 'SitesController')
-  .apiOnly()
-  .middleware({ '*': ['auth'] })
-
-Route.resource('/sites/:siteId/status', 'StatusController')
-  .apiOnly()
-  .middleware({ '*': ['auth'] })
+Route.resource('/sites', 'SitesController').apiOnly().middleware(auth)
+Route.resource('/sites/:siteId/status', 'StatusController').apiOnly().middleware(auth)
